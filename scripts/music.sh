8e11d0ca833afa5bcba9e977f55e90a50e3ddeb1
joeldba <75224242+joeldba@users.noreply.github.com>
2021-06-23 08:42:29 -0700

    added song list option & mpdmenu script

--- scripts/music.sh
+++ scripts/music.sh
@@ -5,9 +5,10 @@
 ###
 
 DMENU='dmenu -i -p music'
-choice=$(echo -e "play\npause\nstop\nnext song\nprev song\ntoggle random\ntoggle single\nmute\nvol 20\nvol 50\nvol 100" | $DMENU | cut -f 1)
+choice=$(echo -e "song list\nplay\npause\nstop\nnext song\nprev song\ntoggle random\ntoggle single\nmute\nvol 35\nvol 50\nvol 100" | $DMENU | cut -f 1)
 
 case "$choice" in
+ "song list") $HOME/.config/dwm/scripts/mpdmenu.sh -p ;; 
  play) mpc play  ;;
  pause) mpc pause  ;;
  stop) mpc stop  ;;
@@ -16,7 +17,7 @@ case "$choice" in
  "toggle random") mpc random ;;
  "toggle single") mpc single ;;
  mute) mpc vol 0 ;;
- "vol 20") mpc vol 20 ;;
+ "vol 35") mpc vol 20 ;;
  "vol 50") mpc vol 50 ;;
  "vol 100") mpc vol 100 ;;
 esac
